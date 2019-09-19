fd746c3f7f51e22c0fc6f6d75bac16e85b24a69a
Berk Karahan <berkkarahan00@gmail.com>
2019-09-20 00:35:28 +0300

    v2 notebooks / scripts

--- v1_scripts/preprocessing.py
+++ v1_scripts/preprocessing.py
@@ -7,23 +7,29 @@ from sklearn.cluster import KMeans
 data = pd.read_csv('data.csv', delimiter=';')
 data.set_index('row_num', inplace=True)
 
+
 def preparedata(df):
     df['set'] = 'train'
-    df.set[df.hits=="\\N"] = 'pred'
-    df.session_durantion[df['session_durantion']=="\\N"]=0
+    df.set[df.hits == "\\N"] = 'pred'
+    df.session_durantion[df['session_durantion'] == "\\N"] = 0
     df.session_durantion = df.session_durantion.astype('int')
-    df.session_durantion[df.session_durantion==0] = df.session_durantion[df.session_durantion!=0].mean()
+    df.session_durantion[df.session_durantion ==
+                         0] = df.session_durantion[df.session_durantion != 0].mean()
     return df
+
 
 def feature_engineering(df):
     df['count_visited'] = df['path_id_set'].str.split(';').str.len()
     df.count_visited.fillna(0, inplace=True)
 
     sc = StandardScaler()
-    df['sd_scaled'] = sc.fit_transform(df['session_durantion'].values.reshape(-1,1))
+    df['sd_scaled'] = sc.fit_transform(
+        df['session_durantion'].values.reshape(-1, 1))
     km = KMeans(n_clusters=2, init='k_means++')
-    df['session_cluster'] = km.fit_transform(df.sd_scaled.values.reshape(-1,1))
+    df['session_cluster'] = km.fit_transform(
+        df.sd_scaled.values.reshape(-1, 1))
     return df
+
 
 def encode_features(df):
     dummy = [
@@ -34,7 +40,7 @@ def encode_features(df):
         'entry_page',
         'traffic_type',
         'session_cluster'
-        ]
+    ]
     dummies = pd.get_dummies(df[dummy])
     df.drop(dummy, axis=1, inplace=True)
     final_df = pd.merge(df, dummies, left_index=True, right_index=True)
@@ -42,6 +48,7 @@ def encode_features(df):
     final_df['row_num'] = final_df.index
     del dummies
     return final_df
+
 
 if __name__ == '__main__':
     data = preparedata(data)
